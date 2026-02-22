32ee88f16412fabf353a05a5c11c946484165bc5
tonicstro <castrotonigomez15@gmail.com>
2026-02-22 22:32:37 +0100

    Segundo commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Portfolio cargado correctamente");
+
+// Animación suave al hacer scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
